7a4eed7600bd2ea6607116dfcc918d0e49f74a28
Elena Anyusheva <baajina@gmail.com>
2026-04-11 15:51:31 +0200

    add header

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="site-header">
+            <div className="container header-inner">
+                <div className="header-logo-group">
+                    <img
+                        className="header-logo"
+                        src="/img/logo.svg"
+                        alt="(не) Ленкин клуб"
+                    />
+                </div>
+                <button className="btn btn--outline">Войти</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
