5e190dd3b139b9f2a0253c16e67acf24d244b7ba
Elena Anyusheva <baajina@gmail.com>
2026-04-29 23:56:48 +0200

    move about section into a separate component

---
+++ src/AboutSection.jsx
@@ -0,0 +1,38 @@
+import './Section.css'
+
+function AboutSection() {
+    return (
+        <section className="section section-about">
+            <div className="container section-about-inner">
+                <div className="about-text-block">
+                    <h1 className="section-title">О клубе</h1>
+
+                    <div className="about-description">
+                        <p>
+                            Клуб про System Engineering: базы данных, распределенные
+                            системы, лоад балансеры, DNS резолверы и всякое такое!
+                        </p>
+                        <ol className="about-list">
+                            <li>читаем Designing Data Intensive Applications</li>
+                            <li>тренируем leetcode собесы</li>
+                        </ol>
+                    </div>
+
+                    <div className="section-actions">
+                        <button className="btn btn--primary">Вступить</button>
+                    </div>
+                </div>
+
+                <div className="about-illustration">
+                    <img
+                        className="about-image"
+                        src="/img/book.svg"
+                        alt="Иллюстрация клуба"
+                    />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutSection
