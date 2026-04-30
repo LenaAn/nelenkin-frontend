11cf51a514fec55c98763f09a72c297a25b44fd2
Elena Anyusheva <baajina@gmail.com>
2026-04-30 02:20:25 +0200

    add actual calendar

--- src/components/sections/CalendarSection.jsx
+++ src/components/sections/CalendarSection.jsx
@@ -7,13 +7,10 @@ function CalendarSection() {
                 <h2 className="section-title section-title--center">
                     Календарь наших событий
                 </h2>
-                <div className="calendar-frame">
-                    <img
-                        className="calendar-image"
-                        src="/img/calendar.png"
-                        alt="Календарь событий"
-                    />
-                </div>
+                <iframe className="calendar-frame"
+                    src="https://calendar.google.com/calendar/embed?src=f9b540541697defb8f20606ae563c7f2bb42e1c16cb3a15e9a887a43fd33acb8%40group.calendar.google.com&ctz=Europe%2FBelgrade"
+                    style={{ border: 0 }} width="800" height="600" frameBorder="0" scrolling="no">
+                </iframe>
             </div>
         </section>
     )
