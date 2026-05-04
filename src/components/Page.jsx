6a3e321848b66c09482b28dd5df6cd1e801778f1
Elena Anyusheva <baajina@gmail.com>
2026-05-04 16:24:33 +0200

    add section about telegram

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,12 +2,14 @@ import './Page.css'
 import AboutSection from "./sections/AboutSection.jsx";
 import HowThisWorksSection from "./sections/HowThisWorksSection.jsx";
 import CalendarSection from "./sections/CalendarSection.jsx";
+import TelegramBotSection from "./sections/TelegramBotSection.jsx"
 
 function Page() {
     return (
         <main className="page-main">
             <AboutSection/>
             <HowThisWorksSection/>
+            <TelegramBotSection/>
             <CalendarSection/>
         </main>
     )
---
+++ src/components/sections/TelegramBotSection.jsx
@@ -0,0 +1,38 @@
+import './Section.css'
+
+function TelegramBotSection() {
+    return (
+        <section className="section section-join">
+            <div className="container section-inner">
+                <div className="section-text-block">
+                    <h2 className="section-title section-title--light">Как это работает?</h2>
+                    <p className="section-description">
+                        Общение происходит в группе в Telegram, еще нам помогает telegram-бот.
+                        <br/><br/>
+                        Бот многое умеет!
+                        <ol className="about-list">
+                            <li>Знает, какие потоки сейчас активны</li>
+                            <li>Присылает ссылку на Zoom звонок за 5 минут до встречи</li>
+                            <li>Делает рассылку новостей клуба, объявляет о новых потоках и сходках в разных
+                                городах
+                            </li>
+                            <li>Следит, чтобы не было анонимов — чтобы вступить в клуб, надо написать интро в боте</li>
+                        </ol>
+                    </p>
+                    <div className="section-actions">
+                        <a href="https://t.me/neLenkin_bot" target="_blank" rel="noopener noreferrer"
+                           className="btn btn--light">к Telegram-боту</a>
+                    </div>
+                </div>
+
+                <img
+                    className="section-image"
+                    src="/img/sparkles.svg"
+                    alt="Декоративные звёздочки"
+                />
+            </div>
+        </section>
+    )
+}
+
+export default TelegramBotSection
