28266b70a38f3e06f46ccd0e15d89151fdfc601a
Elena Anyusheva <baajina@gmail.com>
2026-05-08 16:24:50 +0200

    add a link to my channel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ function Footer() {
     return (
         <footer className="site-footer">
             <div className="container footer-inner">
-                <div className="footer-brand">(не) Ленкин клуб</div>
+                <div className="footer-brand"><a target="_blank"
+                                                 rel="noopener noreferrer"
+                                                 href="http://t.me/lenka_ne_work">(не) Ленкин клуб</a></div>
                 <div className="footer-copy">© 2026</div>
             </div>
         </footer>
