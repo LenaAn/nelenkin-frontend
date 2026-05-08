2cf694c69fd955708e38307a6f098800bdb94767
Elena Anyusheva <baajina@gmail.com>
2026-05-08 12:28:23 +0200

    clicking on logo return on top of the main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import './Header.css'
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <header className="site-header">
             <div className="header-inner">
-                <img
-                    className="header-logo"
-                    src="/img/logo.svg"
-                    alt="(не) Ленкин клуб"
-                />
+                <Link
+                    to="/"
+                    onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                >
+                    <img
+                        className="header-logo"
+                        src="/img/logo.svg"
+                        alt="(не) Ленкин клуб"
+                    />
+                </Link>
                 <a href="https://t.me/neLenkin_bot" target="_blank" rel="noopener noreferrer"
                    className="btn header-btn btn--outline">к Telegram-боту</a>
             </div>
