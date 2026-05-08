30c51426195ed3cb3fb7c6a5e3a6359f7b3109c1
Elena Anyusheva <baajina@gmail.com>
2026-05-08 13:17:25 +0200

    add a banner about new stream

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,13 @@
 import Header from "./Header.jsx";
 import Page from "./Page.jsx"
 import Footer from "./Footer.jsx"
+import AnnouncementBanner from "./AnnouncementBanner.jsx";
+
 
 function Landing() {
     return (
         <div className="page">
+            <AnnouncementBanner />
             <Header/>
             <Page/>
             <Footer/>
