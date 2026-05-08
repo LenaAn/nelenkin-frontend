014b81257e80ef67863c663cde1a583ec1ea00cc
Elena Anyusheva <baajina@gmail.com>
2026-05-08 15:01:02 +0200

    link in banned opens in new window

--- src/components/AnnouncementBanner.jsx
+++ src/components/AnnouncementBanner.jsx
@@ -4,7 +4,10 @@ function AnnouncementBanner() {
     return (
         <div className="announcement-banner">
             <span>
-                Новый поток с 12 мая: 💖 <a href="https://www.engguidebook.com/" className="announcement-link">
+                Новый поток с 12 мая: 💖 <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.engguidebook.com/" className="announcement-link">
                 The Software Engineer's Guidebook</a> 💖
             </span>
         </div>
