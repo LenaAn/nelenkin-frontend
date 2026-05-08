503d504dd153bec0d49e2042b18ae25be2ad60aa
Elena Anyusheva <baajina@gmail.com>
2026-05-08 14:02:29 +0200

    use modal in CTA section

--- src/components/sections/CTASection.jsx
+++ src/components/sections/CTASection.jsx
@@ -1,14 +1,21 @@
 import './Section.css'
+import {useModal} from "../ModalContext.jsx";
 
 function AboutSection({blueSection}) {
+    const { openTelegramModal } = useModal();
+
     return (
         <section className={`section ${blueSection? 'section-blue' : 'section-white'}`}>
             <div className="container section-inner">
                 <div className="section-text-block">
                     <h1 className={`section-title cta-section-title ${blueSection ? 'section-title--light' : ''}`}>Вступить в клуб</h1>
                     <div className="cta-actions section-actions">
-                        <a href="https://t.me/neLenkin_bot" target="_blank" rel="noopener noreferrer"
-                           className={`btn ${blueSection ? 'btn--light' : 'btn--primary'}`}>к Telegram-боту</a>
+                        <button
+                            className={`btn ${blueSection ? 'btn--light' : 'btn--primary'}`}
+                            onClick={openTelegramModal}
+                        >
+                            Вступить в клуб
+                        </button>
                     </div>
                 </div>
             </div>
