4e0e44cf0e8fca2b5dd2fd88c4e119d7f9efebbe
Elena Anyusheva <baajina@gmail.com>
2026-05-08 14:57:30 +0200

    fix active streams

--- src/components/sections/ActiveStreamsSection.jsx
+++ src/components/sections/ActiveStreamsSection.jsx
@@ -8,10 +8,11 @@ function ActiveStreamsSection({blueSection}) {
                     <h2 className={`section-title ${blueSection ? 'section-title--light' : ''}`}>А какие потоки идут
                         прямо сейчас?</h2>
                     <p className="section-description">
-                        Прямо сейчас мы читаем Designing Data-Intensive Applications (второе издание). А также
-                        тренируем алгоритмические собеседования
+                        Активные потоки сейчас:
                     </p>
                     <ol className="about-list section-description">
+                        <li>The Software Engineer's Guidebook (скоро начнется! 💖)</li>
+                        <li>Designing Data-Intensive Applications (второе издание)</li>
                         <li>Разбираем задачи из списка Leetcode-75</li>
                         <li>Каждую неделю устраиваем моки на определенную тему с Leetcode</li>
                     </ol>
